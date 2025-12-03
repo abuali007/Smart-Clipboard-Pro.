5ddb98173476bd90ac4ab208594e6c57832a68b4
LiteXTools <39652643+abuali007@users.noreply.github.com>
2025-12-03 22:31:14 +0300

    Delete Smart Clipboard Pro directory

--- Smart Clipboard Pro/popup/error-reporter.js
+++
@@ -1,9 +0,0 @@
-import { showError } from './toast.js';
-
-export function reportError(context, error, notifyUser = false, fallbackMessage = 'Something went wrong') {
-    const normalizedError = error instanceof Error ? error : new Error(String(error));
-    console.error(`[${context}]`, normalizedError);
-    if (notifyUser) {
-        showError(fallbackMessage);
-    }
-}
